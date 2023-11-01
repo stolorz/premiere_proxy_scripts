cd852c41a418782f9664f58ea7ef5529a9d2da47
stolorz <michal@stolorz.pl>
2023-11-01 09:46:54 +0100

    handle recursive deep dirs

--- convert_all.bat
+++ convert_all.bat
@@ -2,39 +2,74 @@
 @pushd %~dp0
 setlocal enabledelayedexpansion 
 
+
 SET TL="drawtext=text='MPEG':x=10:y=10:fontsize=90:fontcolor=#ffcccc:shadowcolor=black:shadowx=5:shadowy=5"
 SET TR="drawtext=text='MPEG':x=W-th-220:y=10:fontsize=90:fontcolor=#ffcccc:shadowcolor=black:shadowx=5:shadowy=5"
 SET BR="drawtext=text='MPEG':x=W-th-220:y=H-th-10:fontsize=90:fontcolor=#ffcccc:shadowcolor=black:shadowx=5:shadowy=5"
 SET BL="drawtext=text='MPEG':x=10:y=H-th-10:fontsize=90:fontcolor=#ffcccc:shadowcolor=black:shadowx=5:shadowy=5"
 
 
+SET ParentD=%~dp0
 
-FOR /D /r %%I IN (.) DO (
+REM echo "!ParentD!"
+
+FOR /D /r %%I IN (*) DO (
 	
-	SET CurrentD=%%~nI%%~xI
-	SET MpgDir=..\video_mpg\!CurrentD!
+	SET CurrentD=%%I
 
-	IF "!CurrentD!" == "prores_ai"    ( SET MpgDir=..\video_mpg& SET CurrentD=.& echo setting currentD to empty string )
-	IF "!CurrentD!" == "prores_noisy" ( SET MpgDir=..\video_mpg& SET CurrentD=.& echo setting currentD to empty string )
-	IF "!CurrentD!" == "video_ai"     ( SET MpgDir=..\video_mpg& SET CurrentD=.& echo setting currentD to empty string )
-	IF "!CurrentD!" == "video_noisy"  ( SET MpgDir=..\video_mpg& SET CurrentD=.& echo setting currentD to empty string )
+	call:ReplaceText "!CurrentD!" "!ParentD!" "" RelativeD
 
-	echo "!CurrentD!"
+	SET MpgD=!ParentD!..\video_mpg\!RelativeD!
+	
+	REM echo "!CurrentD!"
+	REM echo "!RelativeD!"
+	REM echo "!MpgD!"
 
- 	IF EXIST !MpgDir! (
+	IF EXIST !MpgD! (
 		echo mpg folder already exists 
 	) ELSE (
-		echo creating folder !MpgDir!
-		mkdir !MpgDir!
+		echo creating folder !MpgD!
+		mkdir !MpgD!
 	)
 
 	for %%f in (!CurrentD!\*.mkv;!CurrentD!\*.mov;!CurrentD!\*.mp4;!CurrentD!\*.mpg;!CurrentD!\*.avi) do ( 
 		SET InFile=%%f
-		SET OutFile=!MpgDir!\%%~nf.mov
+		SET OutFile=!MpgD!\%%~nf.mov
 		ECHO "!InFile! -> !OutFile!"
-		ffmpeg -n -i "!InFile!" -s 1920x1080 -vf "!TL!, !TR!, !BR!, !BL!, fps=25" -c:v mpeg2video -q:v 20 -c:a libmp3lame "!OutFile!"
+		ffmpeg -n -i "!InFile!" -s 1280x720 -vf "!TL!, !TR!, !BR!, !BL!, fps=25" -c:v mpeg2video -q:v 20 -c:a libmp3lame "!OutFile!"
 	)
 
  )
 
 @popd
+
+
+
+
+:FUNCTIONS
+@REM FUNCTIONS AREA
+GOTO:EOF
+EXIT /B
+
+:ReplaceText
+::Replace Text In String
+::USE:
+:: CALL:ReplaceText "!OrginalText!" OldWordToReplace NewWordToUse  Result
+::Example
+::SET "MYTEXT=jump over the chair"
+::  echo !MYTEXT!
+::  call:ReplaceText "!MYTEXT!" chair table RESULT
+::  echo !RESULT!
+::
+:: Remember to use the "! on the input text, but NOT on the Output text.
+:: The Following is Wrong: "!MYTEXT!" !chair! !table! !RESULT!
+:: ^^Because it has a ! around the chair table and RESULT
+:: Remember to add quotes "" around the MYTEXT Variable when calling.
+:: If you don't add quotes, it won't treat it as a single string
+::
+set "OrginalText=%~1"
+set "OldWord=%~2"
+set "NewWord=%~3"
+call set OrginalText=%%OrginalText:!OldWord!=!NewWord!%%
+SET %4=!OrginalText!
+GOTO:EOF
